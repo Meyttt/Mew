ca5d826848ca1ec34edb4b99256fe4169adf0675
Your Name <you@example.com>
2016-11-17 10:46:17 +0300

    Добавлен первый шифровальщик

--- src/main/java/Scrambler.java
+++ src/main/java/Scrambler.java
@@ -1,4 +1,7 @@
+import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Random;
 
 /**
  * Created by master on 17.11.2016.
@@ -7,7 +10,11 @@ public class Scrambler {
     String letters="0123456789+=- !.,";
     HashMap<String,String> encoder = new HashMap<String, String>();
     HashMap<String,String> decoder = new HashMap<String, String>();
+    PrimeNumbers primeNumbers = new PrimeNumbers(1000);
+    ArrayList<Integer> primes=primeNumbers.getPrimeNumbers();
+    Integer p,q, phi,e, d;
     public Scrambler(){
+        Random random = new Random();
         for (int i=0; i<letters.length();i++){
             encoder.put(letters.charAt(i)+"", String.format("%02d",i));
             decoder.put(String.format("%02d",i), letters.charAt(i)+"");
@@ -17,6 +24,12 @@ public class Scrambler {
             encoder.put(ch+"",String.format("%02d",i));
             decoder.put(String.format("%02d", i), ch+"");
         }
+        p=primes.get(random.nextInt(primes.size()));
+        q=primes.get(random.nextInt(primes.size()));
+        phi=(p-1)*(q-1);
+        e=primes.get(random.nextInt(50));
+        d=BigInteger.valueOf(e).modInverse(BigInteger.valueOf(phi)).intValue();
+
     }
     public String encode(String originalString){
         String result = "";
@@ -43,6 +56,10 @@ public class Scrambler {
         }
         return result;
     }
+    public ArrayList<Integer> fullencode(String encodedString){
+        return null;
+    }
+
 
     public static void main(String[] args) {
         Scrambler scrambler= new Scrambler();
